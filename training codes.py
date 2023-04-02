7cc56bcb367630d1218997150c42eafd6debb633
noober30 <129619480+noober30@users.noreply.github.com>
2023-04-02 12:27:07 +0200

    Add files via upload

---
+++ training codes.py
@@ -0,0 +1,47 @@
+#úloha 1     (snáď som pochopil dobre zadanie a nezjednodušil som si to zase moc :D )
+
+#for
+Zoznam = a, b, c, d, e =("Text"), 3, 2.5, False, True 
+Zoznam = [("Text"), 3, 2.5, False, True]
+for typ in Zoznam:
+   print (type (typ))
+
+
+print ("*********************************")
+
+#neviem sa dostať do ďaľšieho riadku
+
+print ("*********************************")
+
+
+#úloha_2
+y=0
+while y < 5:
+    if y < 5:
+        y+=1
+        print ("Hodnota je menšia ako 5")
+    print (y)
+    continue
+print ("je rovná 5")
+
+
+#while/////nič lepšie a jednoduchšie ma nenapadlo, chcel si while, máš while :D
+
+g, h, i, j, k = ("Text"), 3, 2.5, False, True
+while g  == ("Text"):
+    print (type("Text"))
+    while h == 3:
+       print (type(3))
+       while i == 2.5:
+           print (type(2.5))
+           while j == False:
+               print (type(False))
+               while k ==  True:
+                    print == (type(True))
+                    continue
+else:
+    print ()
+#nevypisuje mi poslednú hodnotu k, neviem prečo, nechápem 
+    
+
+#
