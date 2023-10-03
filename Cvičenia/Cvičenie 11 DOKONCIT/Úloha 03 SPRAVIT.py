8a65b3de70c5d2234f46f318f9885f1f5c925194
noober30 <129619480+noober30@users.noreply.github.com>
2023-10-03 23:23:21 +0200

    Delete Cvičenia/Cvičenie 11 DOKONCIT directory

--- Cvičenia/Cvičenie 11 DOKONCIT/Úloha 03 SPRAVIT.py
+++
@@ -1,6 +0,0 @@
-# Naprogramuj hru, ktorá bude trénovať tvoj postreh. Každých x milisekúnd sa farebný kruh s polomerom r 
-# presunie na náhodnú pozíciu v ploche.
-#  Keď stihneš kliknúť do tohto kruhu, pripočítaj k nášmu skóre 1 bod. Keď klikneš vedľa, 
-# skóre sa zníži o 1.
-# Aktuálne skóre sa vypisuje niekde v rohu obrazovky (ako grafický objekt create_text()). 
-# Interval a polomer kruhu sa prečítajú zo vstupu od používateľa pri naštartovaní aplikácie.
