87bc882a96c011640869b72327b2064024b15041
noober30 <129619480+noober30@users.noreply.github.com>
2023-10-03 23:08:04 +0200

    Delete Cvičenia/Cvičenie 10 DOKONCIT directory

--- Cvičenia/Cvičenie 10 DOKONCIT/Úloha 04 SPRAVIT.py
+++
@@ -1,2 +0,0 @@
-#Stláčaním malých a veľkých písmen abecedy sa tieto vypisujú nejakým fontom náhodne na obrazovku.
-# Použi metódu bind_all('<Key>', ...) pričom vo viazanej funkcii pracujte s hodnotou `event.char`
